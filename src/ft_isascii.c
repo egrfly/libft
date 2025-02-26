e99dc276eee4947eb24eef40a1f3206d26fca612
egrfly <egrfly@outlook.com>
2025-02-26 14:55:51 +0000

    Sort isascii

--- src/ft_isascii.c
+++ src/ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: emflynn <emflynn@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 20:26:00 by emflynn           #+#    #+#             */
-/*   Updated: 2024/07/02 16:50:22 by emflynn          ###   ########.fr       */
+/*   Updated: 2025/02/26 14:54:46 by emflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 bool	ft_isascii(char c)
 {
-	return ('\0' <= c && c <= '\177');
+	return ('\0' <= (signed char)c && (unsigned char)c <= '\177');
 }
