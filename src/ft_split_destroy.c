15c3e886573066c91e6c1f112560a061542d2bff
egrfly <egrfly@outlook.com>
2025-03-04 04:45:28 +0000

    Sort split destruction

--- src/ft_split_destroy.c
+++ src/ft_split_destroy.c
@@ -6,7 +6,7 @@
 /*   By: emflynn <emflynn@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 09:58:10 by emflynn           #+#    #+#             */
-/*   Updated: 2024/07/02 17:30:59 by emflynn          ###   ########.fr       */
+/*   Updated: 2025/03/04 02:59:11 by emflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	ft_split_destroy(char ***splits)
 	unsigned int	i;
 
 	i = 0;
-	while (*splits[i])
+	while ((*splits)[i])
 	{
-		free(*splits[i]);
+		free((*splits)[i]);
 		i++;
 	}
 	free(*splits);
